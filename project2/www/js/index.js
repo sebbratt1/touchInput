de3f02688cfe394ee401c2523e5d61fb18fe7eba
sebbratt1 <43443754+sebbratt1@users.noreply.github.com>
2019-10-31 14:47:24 +0000

    update

---
+++ project2/www/js/index.js
@@ -0,0 +1,21 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+  
+  	$('#tapholdtext').on("taphold",function(){
+    	$(this).hide();
+ 	});                       
+
+	$('#taptext').on("tap",function(){
+    	$(this).css('color', 'red');
+ 	}); 
+
+	$('#swipetext').on("swipeleft",function(){
+    	$(this).css('color', 'green');
+  	});   
+
+	$('#swipetext').on("swiperight",function(){
+		$(this).css('color', 'blue');
+  	});   
+
+});
